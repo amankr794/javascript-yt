b2a0a99403d6120f3372a646283e4b1033b72904
Amankr <109649639+amankr794@users.noreply.github.com>
2023-08-13 14:21:12 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,34 @@
+const accountId = 144553
+let accountEmail = "aman@google.com"
+var accountPassword = "12345"
+accountCity = "Jiapur"  //not a good method to declare variable reserve memory i js
+let accountState;
+
+/********** Updating User Information ********/
+
+// accountId = 2  // not allowed
+
+console.log(accountId)
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+/*
+    Prefer not to use var to declare variable
+    b/c of issue in block scope and functional scope
+
+    Only use let and const to declare variable in js
+
+    in js no any issue of ;
+
+    in js value of initialized variable is undefined
+*/
+
+
+//Printing information in Tabular Structure 
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
